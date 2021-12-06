3bc0f131cc09f0369ca00c8ddc7de4046d9859cf
Alexandre de Freitas Souza <83618808+AleFreitas@users.noreply.github.com>
2021-12-06 18:01:46 -0300

    Add files via upload

---
+++ ProjetoFinal/principal/Main.java
@@ -0,0 +1,11 @@
+package principal;
+
+public class Main {
+
+	public static void main(String[] args) {
+		select sc = new select();
+		testaBD tb = new testaBD();
+		tb.teste1();
+		sc.selectAll();
+	}
+}
